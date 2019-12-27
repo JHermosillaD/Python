9bf2272e0001d9c36aa4c8adc5e7597562be255e
Hermosilla <hermosilladiaz@gmail.com>
2019-12-26 19:05:11 -0600

    take a video

--- Raspberry/video.py
+++ Raspberry/video.py
@@ -3,9 +3,12 @@ import time
 date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 import picamera
 with picamera.PiCamera() as camera:
+    camera.resolution = (1280,720)
+    camera.framerate = 30
     camera.rotation = 180
+    camera.hflip = True
     camera.start_preview()
-    camera.start_recording("/home/pi/Desktop/video/"+ date + "video.h264")
-    camera.wait_recording(15)
+    camera.start_recording("/home/pi/Documents/Pivideos/"+ date + "video.h264")
+    camera.wait_recording(10)
     camera.stop_recording()
     camera.stop_preview()
